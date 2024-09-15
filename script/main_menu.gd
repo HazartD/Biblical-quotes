0e8ba33ee7f8b7286c5ea814c1285dc097c0bd4f
HazartD <120349577+HazartD@users.noreply.github.com>
2024-09-15 16:43:30 -0600

    Books

--- script/main_menu.gd
+++ script/main_menu.gd
@@ -2,8 +2,11 @@ extends Control
 const album=preload("res://scene/album.tscn")
 const cita=preload("res://scene/cita.tscn")
 const DIR="user://"
-@onready var album_list=$albumes
-@onready var citas=$Citas
+@onready var album_list=$ScrollContainer/albumes
+@onready var citas=$album_name/ScrollContainer2/Citas
+@onready var album_actual=$album_name
+@onready var album_name=$album_name/actual_album
+@onready var album_astrac=$album_name/actual_album_astrac
 var con=ConfigFile.new()
 var actual:String
 func _ready()->void:
@@ -12,12 +15,13 @@ func _ready()->void:
 		new.text=i.erase(i.length()-4,4)
 		new.connect("select",open_album)
 		album_list.add_child(new)
+	DirAccess.make_dir_recursive_absolute("user://config")
 	#con.set_value("1","book","Gn")
 
 func open_album(_name:String)->void:
 	var nam=DIR+_name+".ini"
 	if nam==actual:
-		citas.show()
+		album_actual.show()
 		album_list.hide()
 		print("ya estaba")
 		return
@@ -35,15 +39,17 @@ func open_album(_name:String)->void:
 			new.v_f.text=con.get_value(i,"finalverse")
 			new.note.text=con.get_value(i,"note")
 			new.update_cita_text()
-		citas.show()
+		album_name.text=con.get_value("Data","name")
+		album_astrac.text=con.get_value("Data","astrac")
+		album_actual.show()
 		album_list.hide()
 		actual=nam
 		print("se cargo")
 
 func dis_seleccion_mode()->void:for i in album_list.get_children(): if i.has_signal("select"):i.connect("select",open_album)
 func _on_configuraciones_volver_button_down()->void:
-	if citas.visible:
-		citas.hide()
+	if album_actual.visible:
+		album_actual.hide()
 		album_list.show()
 		save_con()
 	else:
@@ -54,22 +60,41 @@ func save_con()->void:
 	var changed=false
 	for i in citas.get_child_count():
 		var data=citas.get_child(i)
-		if con.get_value(str(i),"book")!=data.book.text:
+		if con.has_section(str(i)):
+			if con.get_value(str(i),"book")!=data.book.text:
+				con.set_value(str(i),"book",data.book.text)
+				changed=true
+			if con.get_value(str(i),"chapter")!=data.cap.text:
+				con.set_value(str(i),"chapter",str(data.cap.text))
+				changed=true
+			if con.get_value(str(i),"initialverse")!=data.v_i.text:
+				con.set_value(str(i),"initialverse",str(data.v_i.text))
+				changed=true
+			if con.get_value(str(i),"finalverse")!=data.v_f.text:
+				con.set_value(str(i),"finalverse",str(data.v_f.text))
+				changed=true
+			if con.get_value(str(i),"note")!=data.note.text:
+				con.set_value(str(i),"note",str(data.note.text))
+				changed=true
+			print("tenia esa seccion")
+		else:
 			con.set_value(str(i),"book",data.book.text)
-			changed=true
-		if con.get_value(str(i),"chapter")!=data.cap.text:
 			con.set_value(str(i),"chapter",str(data.cap.text))
-			changed=true
-		if con.get_value(str(i),"initialverse")!=data.v_i.text:
 			con.set_value(str(i),"initialverse",str(data.v_i.text))
-			changed=true
-		if con.get_value(str(i),"finalverse")!=data.v_f.text:
 			con.set_value(str(i),"finalverse",str(data.v_f.text))
-			changed=true
-		if con.get_value(str(i),"note")!=data.note.text:
 			con.set_value(str(i),"note",str(data.note.text))
 			changed=true
+			print("no tenia esa seccion, se creo")
 	if changed:
 		con.save(actual)
 		print("guardo")
 	else:print("no se guardo")
+
+func _on_crear_album_cita_button_down():
+	if album_actual.visible:
+		var c=cita.instantiate()
+		citas.add_child(c)
+	else:
+		var n=load("res://scene/new_album.tscn")
+		var ne=n.instantiate()
+		add_child(ne)
--- script/new_album.gd
+++ script/new_album.gd
@@ -3,4 +3,4 @@ func _on_button_button_down()->void:
 	var con=ConfigFile.new()
 	con.set_value("DATA","NAME",$name.text)
 	con.set_value("DATA","ASTRAC",$descripcion.text)
-	con.save(Global.DIR+$name.text)
+	con.save("user://"+$name.text+".ini")
